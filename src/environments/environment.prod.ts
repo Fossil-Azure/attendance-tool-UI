bee3015ac3d08ca13e2bc1ed3a8e71b07cfb80e2
Fossil-Azure <fossil.azure@gmail.com>
2024-08-21 11:34:35 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://attendancetrackerdigital.azurewebsites.net'
+    apiUrl: 'https://attendancetrackerdigital-h0hdb0fggsc7a8eg.centralindia-01.azurewebsites.net/'
 };
